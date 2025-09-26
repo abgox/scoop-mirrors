690406078d41dc443609bace5d4da7cd8e699564
abgox <abgohxf@outlook.com>
2025-09-26 21:25:19 +0800

    feat: update sync-github-to-gitee.ps1 script

--- scripts/sync-github-to-gitee.ps1
+++ scripts/sync-github-to-gitee.ps1
@@ -1,26 +1,36 @@
-﻿$repoList = @(
-    "ScoopInstaller/Main",
-    "ScoopInstaller/Extras",
-    "ScoopInstaller/Versions",
-    "matthewjberger/scoop-nerd-fonts",
-    "ScoopInstaller/Nirsoft",
-    "niheaven/scoop-sysinternals",
-    "ScoopInstaller/PHP",
-    "ScoopInstaller/Nonportable",
-    "ScoopInstaller/Java",
-    "Calinou/scoop-games",
-    "ScoopInstaller/Scoop",
-    "ScoopInstaller/Install"
-)
+﻿#Requires -Version 7
+
+$repoJsonPath = "$PSScriptRoot\..\repo.json"
+$repoJsonInfo = Get-Content $repoJsonPath | ConvertFrom-Json
 
 $currentDir = Get-Location
 
-foreach ($repo in $repoList) {
-    Set-Location $currentDir
-    git clone --mirror "git@github.com:$repo.git"
+foreach ($item in $repoJsonInfo.list) {
+    $repoName = $item.repo.Split("/")[-1]
 
-    $repoName = $repo.Split("/")[-1]
+    try {
+        $repoInfo = Invoke-WebRequest "https://api.github.com/repos/$($item.repo)" | ConvertFrom-Json
+    }
+    catch {
+        Write-Host "::error::$_" -ForegroundColor Red
+        continue
+    }
+
+    $clone_url = $repoInfo.clone_url
+    $updated_at = $repoInfo.updated_at
+
+    if ($item.updated_at -eq $updated_at) {
+        Write-Host "Skip $clone_url"
+        continue
+    }
+    else {
+        Write-Host "Sync $clone_url"
+    }
+
+    git clone --mirror $clone_url.Replace("https://github.com", "git@github.com") $repoName
+
     Set-Location $repoName
+
     git remote add gitee "git@gitee.com:scoop-installer-mirrors/$repoName.git"
 
     if ($repoName -eq "Install") {
@@ -29,8 +39,10 @@ foreach ($repo in $repoList) {
     else {
         git push gitee --mirror --force
     }
+
+    $item.updated_at = $updated_at
+
+    Set-Location $currentDir
 }
 
-Set-Location $currentDir
-
-(Get-Date).ToUniversalTime().AddHours(8).ToString('o') | Out-File "last-sync.txt" -Force -Encoding utf8
+$repoJsonInfo | ConvertTo-Json -Depth 100 | Out-File $repoJsonPath -Encoding utf8 -Force
