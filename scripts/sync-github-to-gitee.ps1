6d1bc51d526439848e6f08991731d57173472b61
abgox <abgohxf@outlook.com>
2025-09-26 20:09:59 +0800

    feat: update sync-github-to-gitee.ps1 script

--- scripts/sync-github-to-gitee.ps1
+++ scripts/sync-github-to-gitee.ps1
@@ -10,11 +10,19 @@
     "ScoopInstaller/Java",
     "Calinou/scoop-games",
     "ScoopInstaller/Scoop",
-    "ScoopInstaller/Install",
+    "ScoopInstaller/Install"
 )
 
+$currentDir = Get-Location
+
 foreach ($repo in $repoList) {
+    Set-Location $currentDir
     git clone "https://github.com/$repo.git"
+    Set-Location "$($repo.Split("/")[-1])"
     git remote add gitee "https://gitee.com/scoop-installer-mirrors/$repo.git"
     git push gitee --force
 }
+
+Set-Location $currentDir
+
+(Get-Date).ToUniversalTime().AddHours(8).ToString('o') | Out-File "last-sync.txt" -Force -Encoding utf8
