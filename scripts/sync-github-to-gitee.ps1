41f6cd851195bede0c0833afca05464caf460051
abgox <abgohxf@outlook.com>
2025-09-26 19:35:31 +0800

    feat(scripts): add sync-github-to-gitee.ps1 script

---
+++ scripts/sync-github-to-gitee.ps1
@@ -0,0 +1,20 @@
+﻿$repoList = @(
+    "ScoopInstaller/Main",
+    "ScoopInstaller/Extras",
+    "ScoopInstaller/Versions",
+    "matthewjberger/scoop-nerd-fonts",
+    "ScoopInstaller/Nirsoft",
+    "niheaven/scoop-sysinternals",
+    "ScoopInstaller/PHP",
+    "ScoopInstaller/Nonportable",
+    "ScoopInstaller/Java",
+    "Calinou/scoop-games",
+    "ScoopInstaller/Scoop",
+    "ScoopInstaller/Install",
+)
+
+foreach ($repo in $repoList) {
+    git clone "https://github.com/$repo.git"
+    git remote add gitee "https://gitee.com/scoop-installer-mirrors/$repo.git"
+    git push gitee --force
+}
